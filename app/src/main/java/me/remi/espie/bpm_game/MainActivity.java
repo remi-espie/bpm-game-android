3f9522ba4f5861a41581b07de69846c5437c25f4
Rémi Espié <remi.espie@etu.umontpellier.fr>
2022-10-29 16:26:24 +0200

    transformed into and android app

---
+++ app/src/main/java/me/remi/espie/bpm_game/MainActivity.java
@@ -0,0 +1,19 @@
+package me.remi.espie.bpm_game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
